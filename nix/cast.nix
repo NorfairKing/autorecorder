e079c9e8bbe200e5115482f4d48143a30841e06c
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-10-02 00:48:42 +0200

    A big comment about the copying problem

--- nix/cast.nix
+++ nix/cast.nix
@@ -23,6 +23,23 @@ let
           buildInputs = map (pkg: pkgs."${pkg}") (yamlContents.packages or []);
           buildCommand =
             let
+              # This piece is surprisingly complex.
+              # The spec file can specify a working dir for the cast to happen in, relative to the directory where the spec file is.
+              # When the working dir is lower than the spec file, that working dir needs to exist during the build so that 
+              # autorecorder can cd into it.
+              # When the working dir is higher than the spec file, the parent directories need to exist during the build so that
+              # autorecorder can cd up to them.
+              # The following table describes the situation in all its complexity.
+              # 
+              #  Spec File             | Working dir | Copy             | Casting Start   | Casting `cd`s to | Implemented
+              # -----------------------+-------------+------------------+-----------------+------------------+-------------
+              #  ./foo/bar/spec.yaml   | .           | /build/          | /build/         | /build/          | yes   
+              #  ./foo/bar/spec.yaml   | baz         | /build/baz       | /build/         | /build/baz       | yes   
+              #  ./foo/bar/spec.yaml   | baz/quux    | /build/baz/quux  | /build/         | /build/baz/quux  | yes   
+              #  ./foo/bar/spec.yaml   | ..          | /build/bar       | /build/bar      | /build/          | no   
+              #  ./foo/bar/spec.yaml   | ../..       | /build/foo/bar   | /build/foo/bar  | /build/          | no    
+              #  ./foo/bar/spec.yaml   | ../xyz      | /build/foo/xyz   | /build/bar      | /build/xyz       | no    
+              #
               workingDirScript = pkgs.lib.optionalString (builtins.hasAttr "working-dir" yamlContents) ''
                 mkdir -p ${yamlContents.working-dir}
                 cp -r ${builtins.dirOf src + "/${yamlContents.working-dir}"}/. ${yamlContents.working-dir}/.
